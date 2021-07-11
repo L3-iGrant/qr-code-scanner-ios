b6758a1a2fc62e3ef1cc973b05b8b77c0cc1e792
Mohamed Rebin <rebin@bluecast.ae>
2021-07-11 19:46:51 +0530

    File updates

--- qr-code-scanner-ios/Example/Pods/Target Support Files/Pods-qr-code-scanner-ios_Example/Pods-qr-code-scanner-ios_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_qr_code_scanner_ios_Example : NSObject
-@end
-@implementation PodsDummy_Pods_qr_code_scanner_ios_Example
-@end
--- qr-code-scanner-ios/Example/Pods/Target Support Files/qr-code-scanner-ios/qr-code-scanner-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_qr_code_scanner_ios : NSObject
-@end
-@implementation PodsDummy_qr_code_scanner_ios
-@end
